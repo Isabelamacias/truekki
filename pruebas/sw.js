f5298485a93327b49852610d3a68ab8da40a678d
isabela <you@example.com>
2025-09-17 10:48:05 -0500

    adaptar menu desplegable en producto

---
+++ pruebas/sw.js
@@ -0,0 +1,12 @@
+// sw.js
+const CACHE = "favoritos-cache-v1";
+const FILES = ["./","./index.html"];
+
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+});
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    fetch(e.request).catch(()=>caches.match(e.request))
+  );
+});
